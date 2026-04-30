e4912a597cab90f41431ccecda204a6ffbe8a133
Adam Vessey <adam@discoverygarden.ca>
2026-04-30 14:13:41 -0300

    Fix method signatures.

--- src/DependencyResolver/ImportQueueResolver.php
+++ src/DependencyResolver/ImportQueueResolver.php
@@ -22,7 +22,7 @@ class ImportQueueResolver implements ContentSyncResolverInterface {
    * @param array $normalized_entities
    *   Parsed entities to import.
    */
-  protected function depthFirstSearch(array &$visited, array $identifiers, array $normalized_entities) {
+  protected function depthFirstSearch(array &$visited, array $identifiers, array $normalized_entities, array $serializer_context = [], int $depth = 0) {
     foreach ($identifiers as $identifier) {
 
       // Get a decoded entity. FALSE means no need to import.
@@ -36,7 +36,7 @@ class ImportQueueResolver implements ContentSyncResolverInterface {
       // Process dependencies first.
       if (!empty($entity['_content_sync']['entity_dependencies'])) {
         foreach ($entity['_content_sync']['entity_dependencies'] as $ref_entity_type_id => $references) {
-          $this->depthFirstSearch($visited, $references, $normalized_entities);
+          $this->depthFirstSearch($visited, $references, $normalized_entities, $serializer_context, $depth + 1);
         }
       }
 
@@ -45,7 +45,7 @@ class ImportQueueResolver implements ContentSyncResolverInterface {
         foreach ($entity["_translations"] as $translation) {
           if (!empty($translation['_content_sync']['entity_dependencies'])) {
             foreach ($translation['_content_sync']['entity_dependencies'] as $ref_entity_type_id => $references) {
-              $this->depthFirstSearch($visited, $references, $normalized_entities);
+              $this->depthFirstSearch($visited, $references, $normalized_entities, $serializer_context, $depth + 1);
             }
           }
         }
@@ -114,7 +114,7 @@ class ImportQueueResolver implements ContentSyncResolverInterface {
    * @return array
    *   Queue to be processed within a batch process.
    */
-  public function resolve(array $normalized_entities, $visited = []) {
+  public function resolve(array $normalized_entities, $visited = [], array $serializer_context = []) {
     $visited = [];
     foreach ($normalized_entities as $identifier => $entity) {
       $this->depthFirstSearch($visited, [$identifier], $normalized_entities);
